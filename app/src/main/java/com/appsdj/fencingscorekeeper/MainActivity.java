91724cb867c52f06fad241df23bd201b0a8f9de2
appsdj <iudeant@gmail.com>
2018-01-08 03:00:59 +0000

    First commit: sets backround and initial buttons

---
+++ app/src/main/java/com/appsdj/fencingscorekeeper/MainActivity.java
@@ -0,0 +1,95 @@
+package com.appsdj.fencingscorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // TEAM A scoring methods //
+    /**
+     * Increase score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase score for Team A by1 point.
+     */
+    public void addOneForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+    // END TEAM A scoring methods
+
+    // TEAM B scoring methods
+    /**
+     * Increase score for Team A by 3 points.
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase score for Team A by 2 points.
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase score for Team A by1 point.
+     */
+    public void addOneForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+    // END TEAM B scoring methods
+
+    // Reset scores to 0
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.fencer_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.fencer_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
+
